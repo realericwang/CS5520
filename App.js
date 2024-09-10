0b5184d46aebf2028279fe4c8719df616cc947ac
Eric Wang <eric.ucl@outlook.com>
2024-09-10 14:35:49 -0700

    Add useState for text input

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import Header from './Components/Header';
+import { useState } from 'react';
 
 export default function App() {
-  const appName = 'React Native';
+  const [text, setText] = useState('');
+  const appName = 'React Native NEU 5520';
   return (
     <View style={styles.container}>
-      <Text>Welcome to {appName}</Text>
+      <Header title={appName} >
+        <Text>Child Component</Text>
+       </Header> 
+       <TextInput
+  placeholder="Enter text here"
+  autoCorrect={true}
+  onChangeText={setText}
+  keyboardType="default"
+  value={text}
+  style={{borderBottomColor: 'red', borderBottomWidth: 1}}
+/>
       <StatusBar style="auto" />
     </View>
   );
