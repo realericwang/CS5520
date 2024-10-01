c8122c27fbdaec64e3e2cc3b5543902a3d04c214
Eric Wang <wang.yichen7@northeastern.edu>
2024-10-01 15:25:43 -0700

    feat: add GoalDetails

--- App.js
+++ App.js
@@ -1,6 +1,45 @@
 import React from "react";
 import Home from "./Components/Home";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import GoalDetails from "./Components/GoalDetails";
+import { Button } from "react-native";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <Home />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerStyle: { backgroundColor: "purple" },
+            headerTintColor: "white",
+            title: "My Goals",
+          }}
+        />
+        <Stack.Screen
+          name="Details"
+          component={GoalDetails}
+          options={({ route }) => {
+            return {
+              title: route.params ? route.params.goalData.text : "More Details",
+              headerRight: () => {
+                return (
+                  <Button
+                    title="Warning"
+                    onPress={() => {
+                      console.log("warning");
+                    }}
+                  />
+                );
+              },
+            };
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
--- Components/GoalItem.js
+++ Components/GoalItem.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
-export default function GoalItem({ item, onDelete }) {
+export default function GoalItem({ item, onDelete, pressHandler }) {
   const handleDelete = () => {
     onDelete(item.id);
   };
 
+  function handlePress() {
+    // call a callbackfn received from parent
+    // pass the goal obj back to Home.js
+    pressHandler(item);
+  }
+
   return (
-    <View style={styles.goalItem}>
-      <Text style={styles.goalText}>{item.text}</Text>
-      <Button title="X" onPress={handleDelete} />
+    <View style={styles.textContainer}>
+      <Text style={styles.text}>{goalObj.text}</Text>
+      <Button title="X" color="grey" onPress={handleDelete} />
+      <Button title="i" color="grey" onPress={handlePress} />
     </View>
   );
 }
